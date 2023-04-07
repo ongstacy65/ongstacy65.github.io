3b978031f00ba99a3136070f6a81ca39ac97a6d5
Stacy Ong <ongstacy26@gmail.com>
2023-04-07 18:38:46 +0800

    Remove skill page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Footer } from "./components/Footer";
 
@@ -11,7 +10,6 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-      <Skills />
       <Projects />
       <Footer />
     </div>
